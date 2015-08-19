83e9677843ac1640c49fe5f66e73c3b36a811c2d
smartdoc_jen <smartdoc_jen@bitbucket.org>
2015-08-19 11:15:49 +0800

    Initial Commit

---
+++ MC_QRCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MC_QRCode
+//
+//  Created by xulu on 15/8/19.
+//  Copyright (c) 2015年 _MC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
